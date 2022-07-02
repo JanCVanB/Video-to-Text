1e474674487f4d1d4ce51ee4538c9d4c2745f89e
Jan Van Bruggen <JanCVanB@pm.me>
2022-07-02 01:13:08 -0600

    Smooth color video playback

--- videoToTextColor.py
+++ videoToTextColor.py
@@ -2,15 +2,15 @@ import numpy as np
 import cv2
 import pickle
 import sys
+import time
 
-aspect_ratio = 16 / 9
+aspect_ratio = 1 / 1
+input_fps = 30
+output_fps = 5
 
 #Dimensions of the output in terminal characters
-width = 80
+width = 60
 height = int(width / (2 * aspect_ratio))
-
-
-
 
 #Our characters, and their approximate brightness values
 charSet = " riledWWdelir "
@@ -26,8 +26,8 @@ lerped =  pickle.load( open( "colors.pkl", "rb" ) )
 LUT = np.load("LUT.npy")
 
 # Convert an RGB image to a stream of text with ANSI color codes
-def convertImg(img):
-  line = ""
+def convertImg(img, input_frame_index):
+  frame = ""
   
   for row in img:
     for color in row:
@@ -37,32 +37,55 @@ def convertImg(img):
 
       # Lookup the color index in the RGB lookup table
       idx = LUT[b, g, r]
-  
+
       # Get the ANSI color codes and lerp character
       bg, fg, lerp, rgb = lerped[idx]
 
       char = charSet[lerp]
   
-      line += "%s%c" % (setColor(bg, fg), char)
-    # End each line with a black background to avoid color fringe
-    line += "%s\n" % black
-  
+      frame += "%s%c" % (setColor(bg, fg), char)
+    # Reset colors and end line
+    frame += "\033[0m\n"
+
   # Move the cursor back to the top of the frame to prevent rolling
-  line += "\u001b[%iD\u001b[%iA" % (width, height + 1)
-  return line
+  frame += "\u001b[%iD\u001b[%iA" % (width, height + 1)
 
+  return frame
 
-if len(sys.argv) == 2:
-  cap = cv2.VideoCapture(sys.argv[1])
+def pencils_down():
+  print("\u001b[%iC\u001b[%iB" % (width, height))
 
-  while(cap.isOpened()):  
-    ret, frame = cap.read()
+def main():
+  if len(sys.argv) == 2:
+    print()
+    cap = cv2.VideoCapture(sys.argv[1])
 
-    if frame is None:
-      break
-    
-    img = cv2.resize(frame, (width, height))
-    print(convertImg(img))
-else:
-  print("Expected video file as argument.")
+    skippiness = input_fps // output_fps
+    input_frame_index = 0
+    while(cap.isOpened()):
+      frame_start_time = time.time()
+      ret, frame = cap.read()
+
+      input_frame_index += 1
+      if input_frame_index % skippiness != 0:
+        continue
+
+      if frame is None:
+        break
+
+      img = cv2.resize(frame, (width, height))
+      print(convertImg(img, input_frame_index))
+
+      frame_end_time = time.time()
+      time.sleep((1/output_fps)-(frame_end_time-frame_start_time))
+    pencils_down()
+  else:
+    print("Expected video file as argument.")
+
+if __name__ == "__main__":
+  try:
+    main()
+  except KeyboardInterrupt:
+    pencils_down()
+    sys.exit()
 
